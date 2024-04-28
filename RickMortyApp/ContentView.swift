f4e5431d7b48557138f1006f3a54946874ab707f
Ariel Antonio Fundora López <arielito205@gmail.com>
2024-04-28 07:16:51 +0200

    Initial Commit

---
+++ RickMortyApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RickMortyApp
+//
+//  Created by Ariel Antonio Fundora López on 28/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RickMortyApp/RickMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickMortyAppApp.swift
+//  RickMortyApp
+//
+//  Created by Ariel Antonio Fundora López on 28/4/24.
+//
+
+import SwiftUI
+
+@main
+struct RickMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
